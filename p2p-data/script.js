5c6422d7009c421cbf69b73b60198d0938d475a6
page-atsu <105641884+page-atsu@users.noreply.github.com>
2022-05-29 19:11:21 +0900

    Update script.js

--- p2p-data/script.js
+++ p2p-data/script.js
@@ -5,23 +5,20 @@ const Peer = window.Peer;
   const localText = document.getElementById('js-local-text');
   const connectTrigger = document.getElementById('js-connect-trigger');
   const closeTrigger = document.getElementById('js-close-trigger');
-  const sendTrigger1 = document.getElementById('js-send-trigger1');
-  const sendTrigger2 = document.getElementById('js-send-trigger2');
+  const sendTrigger = document.getElementById('js-send-trigger');
   const remoteId = document.getElementById('js-remote-id');
   const messages = document.getElementById('js-messages');
   const meta = document.getElementById('js-meta');
   const sdkSrc = document.querySelector('script[src*=skyway]');
-  
 
-  
   meta.innerText = `
     UA: ${navigator.userAgent}
     SDK: ${sdkSrc ? sdkSrc.src : 'unknown'}
   `.trim();
 
   const peer = (window.peer = new Peer({
-    //key: window.__SKYWAY_KEY__,
-    key: '39f05a53-160c-4ffd-a468-11cdc3be64ef',
+    key: window.__SKYWAY_KEY__,
+    //key: '39f05a53-160c-4ffd-a468-11cdc3be64ef',
     debug: 3,
   }));
 
@@ -36,10 +33,9 @@ const Peer = window.Peer;
     const dataConnection = peer.connect(remoteId.value);
 
     dataConnection.once('open', async () => {
-      messages.textContent += `=== DataConnection has been opened 1 ===\n`;
+      messages.textContent += `=== DataConnection has been opened ===\n`;
 
-      sendTrigger1.addEventListener('click', onClickSend1);
-      sendTrigger2.addEventListener('click', onClickSend2);
+      sendTrigger.addEventListener('click', onClickSend);
     });
 
     dataConnection.on('data', data => {
@@ -48,29 +44,19 @@ const Peer = window.Peer;
 
     dataConnection.once('close', () => {
       messages.textContent += `=== DataConnection has been closed ===\n`;
-      sendTrigger1.removeEventListener('click', onClickSend1);
-      sendTrigger2.removeEventListener('click', onClickSend2);      
+      sendTrigger.removeEventListener('click', onClickSend);
     });
 
     // Register closing handler
     closeTrigger.addEventListener('click', () => dataConnection.close(true), {
       once: true,
     });
-    let value1 = 1;
-    function onClickSend1() {
-      value1 += 1;
-      const data = value1;
-      //const data = localText.value;
+
+    function onClickSend() {
+      const data = localText.value;
       dataConnection.send(data);
-      messages.textContent = `You1: ${data}\n`;
-      localText.value = '';
-    }
-    function onClickSend2() {
-      value1 -= 1;
-      const data = value1;
-      //const data = localText.value;
-      dataConnection.send(data);
-      messages.textContent = `You1: ${data}\n`;
+
+      messages.textContent += `You: ${data}\n`;
       localText.value = '';
     }
   });
@@ -80,10 +66,9 @@ const Peer = window.Peer;
   // Register connected peer handler
   peer.on('connection', dataConnection => {
     dataConnection.once('open', async () => {
-      messages.textContent += `=== DataConnection has been opened 2 ===\n`;
+      messages.textContent += `=== DataConnection has been opened ===\n`;
 
-      sendTrigger1.addEventListener('click', onClickSend1);
-      sendTrigger2.addEventListener('click', onClickSend2);
+      sendTrigger.addEventListener('click', onClickSend);
     });
 
     dataConnection.on('data', data => {
@@ -92,36 +77,23 @@ const Peer = window.Peer;
 
     dataConnection.once('close', () => {
       messages.textContent += `=== DataConnection has been closed ===\n`;
-      sendTrigger1.removeEventListener('click', onClickSend1);
-      sendTrigger2.removeEventListener('click', onClickSend2);
+      sendTrigger.removeEventListener('click', onClickSend);
     });
 
     // Register closing handler
     closeTrigger.addEventListener('click', () => dataConnection.close(true), {
       once: true,
     });
-    let value2 = 10;
-    function onClickSend1() {
-      //const data = localText.value;
-      //setInterval(() => {
-      value2 += 10;
-      const data = value2;
+
+    function onClickSend() {
+      const data = localText.value;
       dataConnection.send(data);
-      messages.textContent = `You: ${data}\n`;
+
+      messages.textContent += `You: ${data}\n`;
       localText.value = '';
-      //}, 1);
     }
-    function onClickSend2() {
-      //const data = localText.value;
-      //setInterval(() => {
-      value2 -= 10;
-      const data = value2;
-      dataConnection.send(data);
-      messages.textContent = `You: ${data}\n`;
-      localText.value = '';
-      //}, 1);
-    }    
   });
 
   peer.on('error', console.error);
 })();
+
